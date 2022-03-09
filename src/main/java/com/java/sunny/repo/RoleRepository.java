eba4e3d5786e3c4629b6092fa09f715256be8e8e
paritoshsunny <sams.sunny.16@gmail.com>
2022-03-10 04:29:37 +0530

    jwt authentication and authorization by role

---
+++ src/main/java/com/java/sunny/repo/RoleRepository.java
@@ -0,0 +1,14 @@
+package com.java.sunny.repo;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.java.sunny.model.AllRole;
+import com.java.sunny.model.Roles;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Roles, Long> {
+	Optional<Roles> findByName(AllRole name);
+}
