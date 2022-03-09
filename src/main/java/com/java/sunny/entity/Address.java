b8cea47512717912cf56aefd517e2ddaf8b669af
paritoshsunny <sams.sunny.16@gmail.com>
2022-03-10 04:40:27 +0530

    db properties changed

--- src/main/java/com/java/sunny/entity/Address.java
+++ src/main/java/com/java/sunny/entity/Address.java
@@ -1,9 +1,7 @@
 package com.java.sunny.entity;
 
-import java.util.ArrayList;
 import java.util.List;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -11,7 +9,6 @@ import javax.persistence.Id;
 import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.Max;
-import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
