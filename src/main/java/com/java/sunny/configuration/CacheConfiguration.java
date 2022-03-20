b36d05d0ad956866a27c03916e75bf9d55f57da1
paritoshsunny <sams.sunny.16@gmail.com>
2022-03-21 00:59:08 +0530

    ConcurrentMapCacheManager

--- src/main/java/com/java/sunny/configuration/CacheConfiguration.java
+++ src/main/java/com/java/sunny/configuration/CacheConfiguration.java
@@ -1,13 +1,34 @@
 package com.java.sunny.configuration;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.autoconfigure.cache.CacheManagerCustomizer;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
 import org.springframework.context.annotation.Configuration;
+
+import com.java.sunny.service.AddressService;
 
 @Configuration
 @EnableCaching
 public class CacheConfiguration {
 	
+	Logger logger = LoggerFactory.getLogger(AddressService.class);
 	
+	
+	CacheManagerCustomizer<ConcurrentMapCacheManager> customizer(){
+		return new ConcurrentCustomizer();
+	}
+	
+	class ConcurrentCustomizer implements CacheManagerCustomizer<ConcurrentMapCacheManager>{
 
-	
+		@Override
+		public void customize(ConcurrentMapCacheManager cacheManager) {
+			
+			cacheManager.setAllowNullValues(false);
+			cacheManager.setStoreByValue(true);
+			logger.trace("Inside CacheManagerCustomizer");
+		}
+		
+	}
 }
